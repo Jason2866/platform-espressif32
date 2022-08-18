f6adf3ed1f1957c89a0821e38e0b0328241cc480
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2022-08-18 17:42:25 +0200

    use variant to check for solo1

--- builder/frameworks/arduino-solo1.py
+++ builder/frameworks/arduino-solo1.py
@@ -29,6 +29,7 @@ from SCons.Script import DefaultEnvironment, SConscript
 env = DefaultEnvironment()
 board = env.BoardConfig()
 build_core = board.get("build.core", "").lower()
+build_variant = board.get("build.variant", "").lower()
 
 SConscript("_embed_files.py", exports="env")
 
@@ -37,7 +38,7 @@ if build_core == "mbcwb":
         join(DefaultEnvironment().PioPlatform().get_package_dir(
             "framework-arduino-mbcwb"), "tools", "platformio-esp-build.py"))
 
-elif "arduino" in env.subst("$PIOFRAMEWORK") and "arduino-solo1" in env.subst("$PIOFRAMEWORK"):
+elif "arduino" in env.subst("$PIOFRAMEWORK") and build_variant == "esp32solo1":
     SConscript(
         join(DefaultEnvironment().PioPlatform().get_package_dir(
             "framework-arduinoespressif32-solo1"), "tools", "platformio-build.py"))
