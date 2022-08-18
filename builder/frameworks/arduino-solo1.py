60177ea882b2d894cf4968bddcd649f88f1c21a6
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2022-08-18 23:31:19 +0200

    Update arduino-solo1.py

--- builder/frameworks/arduino-solo1.py
+++ builder/frameworks/arduino-solo1.py
@@ -38,10 +38,7 @@ if build_core == "mbcwb":
         join(DefaultEnvironment().PioPlatform().get_package_dir(
             "framework-arduino-mbcwb"), "tools", "platformio-esp-build.py"))
 
-elif "arduino" in env.subst("$PIOFRAMEWORK"):
-#elif "arduino" in env.subst("$PIOFRAMEWORK") and build_variant == "esp32solo1":
-#elif "esp32solo1" in build_variant:
-#elif "espidf" not in env.subst("$PIOFRAMEWORK"):
+elif "arduino" in env.subst("$PIOFRAMEWORK") and "esp32solo1" in build_varian:
     SConscript(
         join(DefaultEnvironment().PioPlatform().get_package_dir(
             "framework-arduinoespressif32-solo1"), "tools", "platformio-build.py"))
