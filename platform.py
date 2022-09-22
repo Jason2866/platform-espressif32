6624603f0570e8d1f8aac95552b491679e44103a
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2022-09-22 11:00:40 +0200

    pop ulp toolchain for c3

--- platform.py
+++ platform.py
@@ -100,6 +100,8 @@ class Espressif32Platform(PlatformBase):
                 self.packages.pop("toolchain-xtensa-%s" % available_mcu, None)
 
         if mcu in ("esp32s2", "esp32s3", "esp32c3"):
+            if mcu == "esp32c3":
+                self.packages.pop("toolchain-esp32ulp", None)
             # RISC-V based toolchain for ESP32C3, ESP32S2, ESP32S3 ULP
             self.packages["toolchain-riscv32-esp"]["optional"] = False
 
