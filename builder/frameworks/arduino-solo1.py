d3732ea67ae5caf5a7c35c6398becd855bcf8214
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2022-09-22 23:06:56 +0200

    Update arduino-solo1.py

--- builder/frameworks/arduino-solo1.py
+++ builder/frameworks/arduino-solo1.py
@@ -38,3 +38,9 @@ if "arduino" in env.subst("$PIOFRAMEWORK") and "-DCORE32SOLO1" in extra_flags:
         join(DefaultEnvironment().PioPlatform().get_package_dir(
             "framework-arduinoespressif32-solo1"), "tools", "platformio-build-solo1.py"))
     env["INTEGRATION_EXTRA_DATA"].update({"application_offset": env.subst("$ESP32_APP_OFFSET")})
+
+elif "espidf" not in env.subst("$PIOFRAMEWORK"):
+    SConscript(
+        join(DefaultEnvironment().PioPlatform().get_package_dir(
+            "framework-arduinoespressif32"), "tools", "platformio-build.py"))
+    env["INTEGRATION_EXTRA_DATA"].update({"application_offset": env.subst("$ESP32_APP_OFFSET")})
