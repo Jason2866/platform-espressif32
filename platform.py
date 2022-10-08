0e0e57aa0d148e0200000a19ea4612e62b44b162
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2022-10-08 14:52:25 +0200

    Update platform.py

--- platform.py
+++ platform.py
@@ -26,36 +26,6 @@ IS_WINDOWS = sys.platform.startswith("win")
 
 
 class Espressif32Platform(PlatformBase):
-    def configure_debug_session(self, debug_config):
-        build_extra_data = debug_config.build_data.get("extra", {})
-        flash_images = build_extra_data.get("flash_images", [])
-        solo1_framework = build_extra_data.get("solo1_framework", False)
-        itead_framework = build_extra_data.get("itead_framework", False)
-
-        ignore_conds = [
-            debug_config.load_cmds != ["load"],
-            not flash_images,
-            not all([os.path.isfile(item["path"]) for item in flash_images]),
-        ]
-
-        if any(ignore_conds):
-            return
-
-        load_cmds = [
-            'monitor program_esp "{{{path}}}" {offset} verify'.format(
-                path=to_unix_path(item["path"]), offset=item["offset"]
-            )
-            for item in flash_images
-        ]
-        load_cmds.append(
-            'monitor program_esp "{%s.bin}" %s verify'
-            % (
-                to_unix_path(debug_config.build_data["prog_path"][:-4]),
-                build_extra_data.get("application_offset", "0x10000"),
-            )
-        )
-        debug_config.load_cmds = load_cmds
-
     def configure_default_packages(self, variables, targets):
         if not variables.get("board"):
             return super().configure_default_packages(variables, targets)
@@ -63,6 +33,10 @@ class Espressif32Platform(PlatformBase):
         board_config = self.board_config(variables.get("board"))
         mcu = variables.get("board_build.mcu", board_config.get("build.mcu", "esp32"))
         frameworks = variables.get("pioframework", [])
+
+        build_extra_data = variables.build_data.get("extra", {})
+        solo1_framework = build_extra_data.get("solo1_framework", False)
+        itead_framework = build_extra_data.get("itead_framework", False)
 
         if solo1_framework:
             self.packages.pop("framework-arduino-ITEAD", None)
@@ -221,3 +195,31 @@ class Espressif32Platform(PlatformBase):
 
         board.manifest["debug"] = debug
         return board
+
+    def configure_debug_session(self, debug_config):
+        build_extra_data = debug_config.build_data.get("extra", {})
+        flash_images = build_extra_data.get("flash_images", [])
+
+        ignore_conds = [
+            debug_config.load_cmds != ["load"],
+            not flash_images,
+            not all([os.path.isfile(item["path"]) for item in flash_images]),
+        ]
+
+        if any(ignore_conds):
+            return
+
+        load_cmds = [
+            'monitor program_esp "{{{path}}}" {offset} verify'.format(
+                path=to_unix_path(item["path"]), offset=item["offset"]
+            )
+            for item in flash_images
+        ]
+        load_cmds.append(
+            'monitor program_esp "{%s.bin}" %s verify'
+            % (
+                to_unix_path(debug_config.build_data["prog_path"][:-4]),
+                build_extra_data.get("application_offset", "0x10000"),
+            )
+        )
+        debug_config.load_cmds = load_cmds
