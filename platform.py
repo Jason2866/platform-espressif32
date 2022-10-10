8da26a426aac52166a9961916c0f32171a39821c
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2022-10-10 10:49:45 +0200

    Update platform.py

--- platform.py
+++ platform.py
@@ -26,34 +26,6 @@ IS_WINDOWS = sys.platform.startswith("win")
 
 
 class Espressif32Platform(PlatformBase):
-    def configure_debug_session(self, debug_config):
-        build_extra_data = debug_config.build_data.get("extra", {})
-        flash_images = build_extra_data.get("flash_images", [])
-
-        ignore_conds = [
-            debug_config.load_cmds != ["load"],
-            not flash_images,
-            not all([os.path.isfile(item["path"]) for item in flash_images]),
-        ]
-
-        if any(ignore_conds):
-            return
-
-        load_cmds = [
-            'monitor program_esp "{{{path}}}" {offset} verify'.format(
-                path=to_unix_path(item["path"]), offset=item["offset"]
-            )
-            for item in flash_images
-        ]
-        load_cmds.append(
-            'monitor program_esp "{%s.bin}" %s verify'
-            % (
-                to_unix_path(debug_config.build_data["prog_path"][:-4]),
-                build_extra_data.get("application_offset", "0x10000"),
-            )
-        )
-        debug_config.load_cmds = load_cmds
-
     def configure_default_packages(self, variables, targets):
         if not variables.get("board"):
             return super().configure_default_packages(variables, targets)
@@ -63,20 +35,20 @@ class Espressif32Platform(PlatformBase):
         core_variant = ''.join(variables.get("board_build.extra_flags", board_config.get("build.extra_flags", "")))
         core_variant = core_variant.replace("-D", " ")
         print ("core_variant: ", core_variant)
-        build_extra_data = debug_config.build_data.get("extra", {})
-        test1 = build_extra_data.get("build_core")
-        print ("build_core: ", test1)
+        #build_extra_data = debug_config.build_data.get("extra", {})
+        #test1 = build_extra_data.get("build_core")
+        #print ("build_core: ", test1)
         frameworks = variables.get("pioframework", [])
 
         if "CORE32SOLO1" in core_variant:
             self.packages.pop("framework-arduino-ITEAD", None)
             self.packages.pop("framework-arduinoespressif32", None)
-#        elif itead_framework:
-#            self.packages.pop("framework-arduino-solo1", None)
-#            self.packages.pop("framework-arduinoespressif32", None)
-#        else:
-#            self.packages.pop("framework-arduino-ITEAD", None)
-#            self.packages.pop("framework-arduino-solo1", None)
+        elif "CORE32ITEAD" in core_variant:
+            self.packages.pop("framework-arduino-solo1", None)
+            self.packages.pop("framework-arduinoespressif32", None)
+        else:
+            self.packages.pop("framework-arduino-ITEAD", None)
+            self.packages.pop("framework-arduino-solo1", None)
 
         if "buildfs" in targets:
             filesystem = variables.get("board_build.filesystem", "spiffs")
@@ -225,3 +197,31 @@ class Espressif32Platform(PlatformBase):
 
         board.manifest["debug"] = debug
         return board
+
+    def configure_debug_session(self, debug_config):
+        build_extra_data = debug_config.build_data.get("extra", {})
+        flash_images = build_extra_data.get("flash_images", [])
+
+        ignore_conds = [
+            debug_config.load_cmds != ["load"],
+            not flash_images,
+            not all([os.path.isfile(item["path"]) for item in flash_images]),
+        ]
+
+        if any(ignore_conds):
+            return
+
+        load_cmds = [
+            'monitor program_esp "{{{path}}}" {offset} verify'.format(
+                path=to_unix_path(item["path"]), offset=item["offset"]
+            )
+            for item in flash_images
+        ]
+        load_cmds.append(
+            'monitor program_esp "{%s.bin}" %s verify'
+            % (
+                to_unix_path(debug_config.build_data["prog_path"][:-4]),
+                build_extra_data.get("application_offset", "0x10000"),
+            )
+        )
+        debug_config.load_cmds = load_cmds
