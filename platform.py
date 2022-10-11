e2571057474eb754bbd921d4fd2469d9b651c81e
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2022-10-11 12:33:18 +0200

    Update platform.py

--- platform.py
+++ platform.py
@@ -33,23 +33,21 @@ class Espressif32Platform(PlatformBase):
         buildflags = variables.get("build_flags", [])
         core_variant_build = ''.join(buildflags)
         core_variant_build = core_variant_build.replace("-D", " ")
-        print ("core_variant_build: ", core_variant_build)
+        #print ("core_variant_build: ", core_variant_build)
         board_config = self.board_config(variables.get("board"))
         mcu = variables.get("board_build.mcu", board_config.get("build.mcu", "esp32"))
         core_variant_board = ''.join(variables.get("board_build.extra_flags", board_config.get("build.extra_flags", "")))
         core_variant_board = core_variant_board.replace("-D", " ")
-        print ("Extra_flags core_variant_board: ", core_variant_board)
+        #print ("Extra_flags core_variant_board: ", core_variant_board)
         frameworks = variables.get("pioframework", [])
 
+        self.packages.pop("framework-arduinoespressif32", None)
         if "CORE32SOLO1" in core_variant_board or "FRAMEWORK_ARDUINO_SOLO1" in core_variant_build:
-            self.packages.pop("framework-arduino-ITEAD", None)
-            self.packages.pop("framework-arduinoespressif32", None)
+            self.packages["framework-arduino-solo1"]["optional"] = False
         elif "CORE32ITEAD" in core_variant_board or "FRAMEWORK_ARDUINO_ITEAD" in core_variant_build:
-            self.packages.pop("framework-arduino-solo1", None)
-            self.packages.pop("framework-arduinoespressif32", None)
+            self.packages["framework-arduino-ITEAD"]["optional"] = False
         else:
-            self.packages.pop("framework-arduino-ITEAD", None)
-            self.packages.pop("framework-arduino-solo1", None)
+            self.packages["framework-arduinoespressif32"]["optional"] = False
 
         if "buildfs" in targets:
             filesystem = variables.get("board_build.filesystem", "spiffs")
@@ -63,10 +61,6 @@ class Espressif32Platform(PlatformBase):
             self.packages["tool-openocd-esp32"]["optional"] = False
         if os.path.isdir("ulp"):
             self.packages["toolchain-esp32ulp"]["optional"] = False
-
-        build_core = variables.get(
-            "board_build.core", board_config.get("build.core", "arduino")
-        ).lower()
 
         if "espidf" in frameworks:
             # Common packages for IDF and mixed Arduino+IDF projects
@@ -226,4 +220,3 @@ class Espressif32Platform(PlatformBase):
             )
         )
         debug_config.load_cmds = load_cmds
-
