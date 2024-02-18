f94d6d766eeb3d3faf201851879913d3ad928de5
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2024-02-18 22:51:06 +0100

    Update platform.py

--- platform.py
+++ platform.py
@@ -50,7 +50,7 @@ class Espressif32Platform(PlatformBase):
                     self.packages["framework-arduinoespressif32"]["version"] = "https://codeload.github.com/espressif/arduino-esp32/zip/bc769fd35a1d4ee26f453e9965412b7e3a8d2dc8"
                     self.packages["framework-espidf"]["owner"] = "platformio"
                     self.packages["framework-espidf"]["version"] = "~3.50102.0"
-                    board_config("frameworks") = frameworks
+                    board_config("frameworks").update(frameworks)
                     frameworks_board = variables.get("board.frameworks", board_config.get("frameworks"))
                     frmwrk_board = board_config.get("frameworks")
                     print("frameworks_board: ", frameworks_board)
