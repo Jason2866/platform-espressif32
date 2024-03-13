9621b954de1d865df02216f33f45d19d3aec8039
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2024-03-13 15:24:41 +0100

    orig IDF 5.1.3.240313

--- platform.py
+++ platform.py
@@ -54,7 +54,7 @@ class Espressif32Platform(PlatformBase):
                 self.packages["framework-arduinoespressif32"]["version"] = "https://github.com/espressif/arduino-esp32/archive/refs/heads/master.zip"
                 self.packages["framework-arduinoespressif32"]["optional"] = False
                 self.packages["framework-espidf"]["owner"] = "jason2866"
-                self.packages["framework-espidf"]["version"] = "https://github.com/Jason2866/esp-idf/releases/download/v5.1.3/esp-idf-v5.1.3.zip" 
+                self.packages["framework-espidf"]["version"] = "https://github.com/Jason2866/esp-idf/releases/download/v5.1.3.240313/esp-idf-v5.1.3.zip" 
             else:
                 self.packages["framework-arduinoespressif32"]["optional"] = False
 
