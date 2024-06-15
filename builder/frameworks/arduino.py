8720cce2788171158babe740c2becd07b77d4b9f
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2024-06-15 14:16:54 +0200

    simplify

--- builder/frameworks/arduino.py
+++ builder/frameworks/arduino.py
@@ -36,15 +36,11 @@ build_core = board.get("build.core", "").lower()
 SConscript("_embed_files.py", exports="env")
 
 if "espidf" not in env.subst("$PIOFRAMEWORK"):
-    SConscript(
-        join(DefaultEnvironment().PioPlatform().get_package_dir(
-            "framework-arduinoespressif32"), "tools", "platformio-build.py"))
     FRAMEWORK_DIR = platform.get_package_dir("framework-arduinoespressif32")
     NIMBLE_DIR = "".join(platform.get_package_dir("esp-nimble-cpp"))
     FRAMEWORK_LIBRARY_DIR = join(FRAMEWORK_DIR, "libraries")
     TARGET_NIMBLE_DIR = join(FRAMEWORK_LIBRARY_DIR, "esp-nimble-cpp")
-    print("Framework Lib Dir: ", FRAMEWORK_LIBRARY_DIR)
-    print("Target Dir: ", TARGET_NIMBLE_DIR)
+    SConscript(join(FRAMEWORK_DIR, "tools", "platformio-build.py"))
     if os.path.exists(FRAMEWORK_LIBRARY_DIR):
         if not os.path.exists(TARGET_NIMBLE_DIR):
             shutil.copytree(NIMBLE_DIR, FRAMEWORK_LIBRARY_DIR)
