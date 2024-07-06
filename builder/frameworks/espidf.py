9b734b3338d762ee794c5fba1f7830f55489a517
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2024-07-06 23:13:55 +0200

    Update espidf.py

--- builder/frameworks/espidf.py
+++ builder/frameworks/espidf.py
@@ -1231,7 +1231,8 @@ def get_idf_venv_dir():
     # unnecessary reinstallation of Python dependencies in cases when Arduino
     # as an IDF component requires a different version of the IDF package and
     # hence a different set of Python deps or their versions
-    idf_version_long = ''.join(get_original_version(platform.get_package_version("framework-espidf")))
+    idf_version_long = str(get_original_version(platform.get_package_version("framework-espidf")))
+    print("idf version long: ", idf_version_long)
     cut_pos = str.find(idf_version_long, "+")
     idf_version = idf_version_long[:cut_pos]
     print("idf version: ", idf_version)
