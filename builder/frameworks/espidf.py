e68b90b01a25d8c00340b4382d423e341301bae6
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2024-09-15 21:05:09 +0200

    Update espidf.py

--- builder/frameworks/espidf.py
+++ builder/frameworks/espidf.py
@@ -250,11 +250,6 @@ def populate_idf_env_vars(idf_env):
         os.path.dirname(get_python_exe()),
     ]
 
-    if mcu in ("esp32", "esp32s2", "esp32s3"):
-        additional_packages.append(
-            os.path.join(platform.get_package_dir("esp32ulp-elf"), "bin"),
-        )
-
     idf_env["PATH"] = os.pathsep.join(additional_packages + [idf_env["PATH"]])
 
     # Some users reported that the `IDF_TOOLS_PATH` var can seep into the
