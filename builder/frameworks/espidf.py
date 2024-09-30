846cbc907db47197af4f072435fd976a23554370
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2024-09-30 17:52:26 +0200

    comment

--- builder/frameworks/espidf.py
+++ builder/frameworks/espidf.py
@@ -120,12 +120,13 @@ try:
         ORIG_BUILD_UNFLAGS = env.subst("$BUILD_UNFLAGS")
         ORIG_LINKFLAGS = env.subst("$LINKFLAGS")
         ORIG_PROJECT_SRC_DIR = PROJECT_SRC_DIR
-        env.GetProjectOption("build_flags").clear()
-        env.GetProjectOption("build_unflags").clear()
+        #env.GetProjectOption("build_flags").clear()
+        #env.GetProjectOption("build_unflags").clear()
         #env.GetProjectOption("linkflags").clear()
-        PROJECT_SRC_DIR = PROJECT_SRC_DIR.replace("tasmota", "dummy")
+        NEW_PROJECT_SRC_DIR = PROJECT_SRC_DIR.replace("tasmota", "dummy")
+        PROJECT_SRC_DIR = NEW_PROJECT_SRC_DIR
         env.Replace(
-            PROJECT_SRC_DIR=PROJECT_SRC_DIR,
+            PROJECT_SRC_DIR=NEW_PROJECT_SRC_DIR,
             BUILD_FLAGS="-fno-lto",
             BUILD_UNFLAGS="",
             LINKFLAGS="-fno-lto",
