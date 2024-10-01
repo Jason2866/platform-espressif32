562231df7878823c046bcb4b27a91163ddd2b1e4
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2024-10-01 18:38:01 +0200

    Update arduino.py

--- builder/frameworks/arduino.py
+++ builder/frameworks/arduino.py
@@ -54,7 +54,7 @@ except:
     flag_custom_sdkonfig = False
 
 if flag_custom_sdkonfig:
-    if not env["INTEGRATION_EXTRA_DATA"].get({"arduino_lib_compile_flag"}):
+    if "True" not in env["INTEGRATION_EXTRA_DATA"].get({"arduino_lib_compile_flag"}):
         print("Arduino libs compile flag", env["INTEGRATION_EXTRA_DATA"].get({"arduino_lib_compile_flag"}))
         #not (["idf_libs_compiled"] == env.GetProjectOption("custom_sdkconfig")):
         print("Arduino IDF libs compile")
