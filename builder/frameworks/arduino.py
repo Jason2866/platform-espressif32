9e671f598a31479cdcc61141cf6e5e31cf5183ab
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2024-10-01 12:21:49 +0200

    start IDF script to build IDF libs for Arduino

--- builder/frameworks/arduino.py
+++ builder/frameworks/arduino.py
@@ -46,6 +46,10 @@ elif "arduino" in env.subst("$PIOFRAMEWORK") and "CORE32SOLO1" not in extra_flag
     FRAMEWORK_DIR = platform.get_package_dir("framework-arduinoespressif32")
 ARDUINO_FRAMEWORK_DIR = FRAMEWORK_DIR
 
+if variables.get("custom_sdkconfig") is not None:
+    if not (["idf_libs_compiled"] == variables.get("custom_sdkconfig")):
+        SConscript("espidf.py")
+
 if "espidf" not in env.subst("$PIOFRAMEWORK"):
     SConscript(join(FRAMEWORK_DIR, "tools", "platformio-build.py"))
 
