f69bfa3ddea4ddee06b4839f2ebb874da11dcb52
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2024-10-01 14:20:15 +0200

    syntax...

--- builder/frameworks/arduino.py
+++ builder/frameworks/arduino.py
@@ -49,7 +49,7 @@ ARDUINO_FRAMEWORK_DIR = FRAMEWORK_DIR
 #if env.GetProjectOption("custom_sdkconfig") is not None:
 try:
     if not (["idf_libs_compiled"] == env.GetProjectOption("custom_sdkconfig")):
-        ["$PIOFRAMEWORK"].append("espidf")
+        env["PIOFRAMEWORK"].append("espidf")
         print("Arduino IDF libs compile")
         print("arduino.py script calling SConscript espidf.py")
         print("Pio framework", env.subst("$PIOFRAMEWORK"))
