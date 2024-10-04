5ed090c8d55d436caa6449306ed4c27cadeb084b
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2024-10-04 13:03:05 +0200

    Update espidf.py

--- builder/frameworks/espidf.py
+++ builder/frameworks/espidf.py
@@ -52,7 +52,6 @@ if os.environ.get("PYTHONPATH"):
     del os.environ["PYTHONPATH"]
 
 env = DefaultEnvironment()
-init_env = env.Clone()
 env.SConscript("_embed_files.py", exports="env")
 
 # Allow changes in folders of managed components
@@ -1904,8 +1903,8 @@ if "arduino" in env.get("PIOFRAMEWORK") and "espidf" not in env.get("PIOFRAMEWOR
         # print("Platform dir", os.path.join(env.subst("$PROJECT_CORE_DIR"), "platforms"))
         # SConscript(os.path.join(env.subst("$PROJECT_CORE_DIR"), "platforms", "builder", "frameworks", "arduino.py"))
         SConscript(join(ARDUINO_FRAMEWORK_DIR, "tools", "platformio-build.py"))
-    env.AddPostAction("checkprogsize", after_build)
-    # env.AddPostAction("build_bootloader", after_build)
+    env.AddPostAction("target_buildprog", after_build)
+    # env.AddPostAction("checkprogsize", after_build)
 
 #
 # Process OTA partition and image
