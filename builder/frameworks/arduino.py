7de892969d9d8c64545ea93d1f72b9b05e71ead3
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2024-10-26 23:31:35 +0200

    Update arduino.py

--- builder/frameworks/arduino.py
+++ builder/frameworks/arduino.py
@@ -63,13 +63,14 @@ FRAMEWORK_DIR = platform.get_package_dir("framework-arduinoespressif32")
 flag_any_custom_sdkconfig = os.path.exists(join(FRAMEWORK_DIR,"tools","esp32-arduino-libs","sdkconfig"))
 
 if flag_custom_sdkconfig and "CORE32SOLO1" in extra_flags and "CONFIG_FREERTOS_UNICORE=y" in (''.join([element for element in config.get("env:"+env["PIOENV"], "custom_sdkconfig")])):
-    # custom_sdkconfig = env.GetProjectOption("custom_sdkconfig")
-    custom_sdkconfig = env.GetProjectConfig().parse_multi_values(env.GetProjectOption('custom_sdkconfig'))
+    env.Append(GetProjectOption=[("custom_sdkconfig", "# CONFIG_SPIRAM is not set")])
+    # custom_sdkconfig = env.GetProjectConfig().parse_multi_values(env.GetProjectOption('custom_sdkconfig'))
+    custom_sdkconfig = env.GetProjectOption("custom_sdkconfig")
     print("custom_sdkconfig", custom_sdkconfig)
     # custom_sdkconfig.update("custom_sdkconfig", ["# CONFIG_SPIRAM is not set"])
-    config.get("env:"+env["PIOENV"], "custom_sdkconfig", "# CONFIG_SPIRAM is not set")
-    custom_sdkconfig_entry = ''.join([element for element in config.get("env:"+env["PIOENV"], "custom_sdkconfig")])
-    print("custom_sdkconfig_entry", custom_sdkconfig_entry)
+    # config.get("env:"+env["PIOENV"], "custom_sdkconfig", "# CONFIG_SPIRAM is not set")
+    # custom_sdkconfig_entry = ''.join([element for element in config.get("env:"+env["PIOENV"], "custom_sdkconfig")])
+    # print("custom_sdkconfig_entry", custom_sdkconfig_entry)
  
 
 def get_MD5_hash(phrase):
