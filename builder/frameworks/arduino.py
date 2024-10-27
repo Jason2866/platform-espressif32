91d4058e92b5ded585e5b29d5c2c7b392812f1d1
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2024-10-27 15:49:39 +0100

    Update arduino.py

--- builder/frameworks/arduino.py
+++ builder/frameworks/arduino.py
@@ -184,6 +184,7 @@ def install_python_deps():
         )
     return
 
+install_python_deps()
+
 if "arduino" in env.subst("$PIOFRAMEWORK") and "espidf" not in env.subst("$PIOFRAMEWORK") and env.subst("$ARDUINO_LIB_COMPILE_FLAG") in ("Inactive", "True"):
-    install_python_deps()
     SConscript(join(FRAMEWORK_DIR, "tools", "platformio-build.py"))
