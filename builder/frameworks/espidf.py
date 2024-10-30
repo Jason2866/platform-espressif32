1b6f4776e7bbb972dd12267b6c4677467744acd0
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2024-10-30 15:34:57 +0100

    Update espidf.py

--- builder/frameworks/espidf.py
+++ builder/frameworks/espidf.py
@@ -207,14 +207,14 @@ def HandleArduinoCOMPONENTsettings(env):
         if idf_custom_component_add != "":
             def write_json(new_data, filename=os.path.join(ARDUINO_FRAMEWORK_DIR, "idf_component.json")):
                 with open(filename,'r+') as file:
-                # First we load existing data into a dict.
-                file_data = json.load(file)
-                # Join new_data with file_data inside emp_details
-                file_data["dependencies"].append(new_data)
-                # Sets file's current position at offset.
-                file.seek(0)
-                # convert back to json.
-                json.dump(file_data, file, indent = 4)
+                    # First we load existing data into a dict.
+                    file_data = json.load(file)
+                    # Join new_data with file_data inside emp_details
+                    file_data["dependencies"].append(new_data)
+                    # Sets file's current position at offset.
+                    file.seek(0)
+                    # convert back to json.
+                    json.dump(file_data, file, indent = 4)
 
             for entry in idf_custom_component_add:
                 # add entrys to json
