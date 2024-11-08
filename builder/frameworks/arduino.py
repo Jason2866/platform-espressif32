0330bd7d68662599e38601ca5d3101c0d682ab86
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2024-11-08 16:13:51 +0100

    Update arduino.py

--- builder/frameworks/arduino.py
+++ builder/frameworks/arduino.py
@@ -65,8 +65,7 @@ if "framework-arduinoespressif32" in FRAMEWORK_DIR:
 if flag_custom_sdkconfig and "CORE32SOLO1" in extra_flags and "CONFIG_FREERTOS_UNICORE=y" in env.GetProjectOption("custom_sdkconfig"):
     if len(str(env.GetProjectOption("build_unflags"))) < 3:
         env['BUILD_UNFLAGS'] = {}
-        env.Replace(BUILD_UNFLAGS=" -DJUST_A_TEST")
-        print("BUILD_UNFLAGS", "".join(env['BUILD_UNFLAGS']))
+        print("BUILD_UNFLAGS", env['BUILD_UNFLAGS'])
     build_unflags = " ".join(env['BUILD_UNFLAGS'])
     build_unflags = build_unflags + " -mdisable-hardware-atomics -ustart_app_other_cores"
     print("BUILD_UNFLAGS", build_unflags)
