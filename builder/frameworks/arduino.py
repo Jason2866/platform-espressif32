98dc048f1e34fb51ebfccafaa9875f4696ba0be6
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2024-11-08 16:28:38 +0100

    Update arduino.py

--- builder/frameworks/arduino.py
+++ builder/frameworks/arduino.py
@@ -65,6 +65,7 @@ if "framework-arduinoespressif32" in FRAMEWORK_DIR:
 if flag_custom_sdkconfig and "CORE32SOLO1" in extra_flags and "CONFIG_FREERTOS_UNICORE=y" in env.GetProjectOption("custom_sdkconfig"):
     
     print("**** Länge env", len(str(env.GetProjectOption("build_unflags"))))
+    print("**** Inhalt env", env.GetProjectOption("build_unflags"))
     if len(str(env.GetProjectOption("build_unflags"))) < 3:
         env['BUILD_UNFLAGS'] = {}
         print("BUILD_UNFLAGS", env['BUILD_UNFLAGS'])
