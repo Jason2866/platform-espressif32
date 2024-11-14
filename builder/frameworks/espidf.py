54639ed70dd0f3f4915304834786e48c18536a54
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2024-11-14 21:44:25 +0100

    Update espidf.py

--- builder/frameworks/espidf.py
+++ builder/frameworks/espidf.py
@@ -199,27 +199,21 @@ def HandleArduinoIDFsettings(env):
             return ""
         sdkconfig_entrys = env.GetProjectOption("custom_sdkconfig").splitlines()
         for file in sdkconfig_entrys:
-            print("*** Parser:", file)
             if "http" in file and "://" in file:
                 response = requests.get(file.split(" ")[0])
                 if response.ok:
                     target = str(response.content.decode('utf-8'))
                 else:
-                    print("Failed to download: ",file)
+                    print("Failed to download:", file)
                     return ""
                 return target
             if "file://" in file:
-                # PROJECT_ROOT_DIR = PROJECT_DIR.rsplit(os.path.sep,1)[0]
-                # print("*** project root dir", PROJECT_ROOT_DIR)
                 file_path = join(PROJECT_DIR,file.lstrip("file://").split(os.path.sep)[-1])
-                print("*** project dir list", os.listdir(PROJECT_DIR))
-                # print("*** root dir list", os.listdir(PROJECT_ROOT_DIR))
-                print("custom sdkconfig file path:", file_path)
-                if len(file_path) > 1:
+                if os.path.exists(file_path):
                     with open(file_path, 'r') as file:
                         target = file.read()
                 else:
-                    print("File not found: ",file)
+                    print("File not found:", file_path)
                     return ""
                 return target
         return ""
@@ -232,8 +226,7 @@ def HandleArduinoIDFsettings(env):
     if config.has_option("env:"+env["PIOENV"], "custom_sdkconfig"):
         flag_custom_sdkonfig = True
         custom_sdk_config_flags = (env.GetProjectOption("custom_sdkconfig").rstrip("\n")) + "\n"
-        custom_sdkconfig_file = custom_sdkconfig_file(sdkconfig_file_flags)
-        print("*** custom_sdkconfig_file", custom_sdkconfig_file)
+        custom_sdkconfig_file_str = custom_sdkconfig_file(sdkconfig_file_flags)
 
     if "espidf.custom_sdkconfig" in board:
         board_idf_config_flags = ('\n'.join([element for element in board.get("espidf.custom_sdkconfig", "")])).rstrip("\n") + "\n"
@@ -242,8 +235,8 @@ def HandleArduinoIDFsettings(env):
     if flag_custom_sdkonfig == True: # TDOO duplicated
         print("*** Add \"custom_sdkconfig\" settings to IDF sdkconfig.defaults ***")
         idf_config_flags = custom_sdk_config_flags
-        if custom_sdkconfig_file != "":
-            sdkconfig_file_flags = custom_sdkconfig_file(sdkconfig_file_flags) + "\n"
+        if custom_sdkconfig_file_str != "":
+            sdkconfig_file_flags = custom_sdkconfig_file_str + "\n"
             idf_config_flags = idf_config_flags + sdkconfig_file_flags
         idf_config_flags = idf_config_flags + board_idf_config_flags
         if flash_frequency != "80m":
