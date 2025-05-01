7bec63d93556b2809018724e5778663ce55b7365
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2025-05-01 17:02:07 +0200

    refactor check tools install

--- platform.py
+++ platform.py
@@ -104,10 +104,17 @@ class Espressif32Platform(PlatformBase):
         if tl_flag:
             # Install tool is not needed anymore
             del self.packages["tl-install"]
-            # Enable check tools only when "check_tool" is enabled
-            for p in self.packages:
-                if p in ("tool-cppcheck", "tool-clangtidy", "tool-pvs-studio"):
-                    self.packages[p]["optional"] = False if str(variables.get("check_tool")).strip("['']") in p else True
+            CHECK_PACKAGES = [
+                "tool-cppcheck",
+                "tool-clangtidy",
+                "tool-pvs-studio"
+            ]
+            # Install check tool listed in pio entry "check_tool"
+            if variables.get("check_tool") is not None:
+                for package in CHECK_PACKAGES:
+                    for check_tool in variables.get("check_tool", ""):
+                        if check_tool in package:
+                            self.packages[package]["optional"] = False
 
         if "arduino" in frameworks:
             self.packages["framework-arduinoespressif32"]["optional"] = False
