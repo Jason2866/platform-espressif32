a022685fd2320f96e543127a754d078b1c658e05
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2025-05-01 15:46:33 +0200

    Update platform.py

--- platform.py
+++ platform.py
@@ -146,11 +146,11 @@ class Espressif32Platform(PlatformBase):
                 self.packages[package]["optional"] = False
 
         # Install check tool listed in pio entry "check_tool"
-        if len(str(variables.get("check_tool"))) > 3:
+        if variables.get("check_tool") is not None:
             print("list check_tool:", variables.get("check_tool"))
             for p in self.packages:
                 if p in ("tool-cppcheck", "tool-clangtidy", "tool-pvs-studio"):
-                    for check_tool in variables.get("check_tool", ""):
+                    if check_tool in variables.get("check_tool", ""):
                         print("check tool:", check_tool)
                         print("p:", p)
                         self.packages[p]["optional"] = False
