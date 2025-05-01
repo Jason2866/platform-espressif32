aaee7723d1534de24d2f0acea67fbba73f512dc2
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2025-05-01 15:59:33 +0200

    Update platform.py

--- platform.py
+++ platform.py
@@ -148,13 +148,13 @@ class Espressif32Platform(PlatformBase):
         # Install check tool listed in pio entry "check_tool"
         if variables.get("check_tool") is not None:
             print("list check_tool:", variables.get("check_tool"))
-            for p in self.packages:
-                if p in ("tool-cppcheck", "tool-clangtidy", "tool-pvs-studio"):
-                    for check_tool in variables.get("check_tool", ""):
-                        if check_tool in p:
-                            print("check tool:", check_tool)
-                            print("p:", p)
-                            self.packages[p]["optional"] = False
+            #for p in self.packages:
+            for p in ("tool-cppcheck", "tool-clangtidy", "tool-pvs-studio"):
+                for check_tool in variables.get("check_tool", ""):
+                    if check_tool in p:
+                        print("check tool:", check_tool)
+                        print("p:", p)
+                        self.packages[p]["optional"] = False
 
         if "buildfs" in targets:
             filesystem = variables.get("board_build.filesystem", "littlefs")
