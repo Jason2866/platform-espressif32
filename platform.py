ee45adfee5806f1bd4466cc98135e1f9aeea1d0d
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2025-05-07 17:22:39 +0200

    Update platform.py

--- platform.py
+++ platform.py
@@ -43,6 +43,9 @@ class Espressif32Platform(PlatformBase):
         board_config = self.board_config(variables.get("board"))
         mcu = variables.get("board_build.mcu", board_config.get("build.mcu", "esp32"))
         board_sdkconfig = variables.get("board_espidf.custom_sdkconfig", board_config.get("espidf.custom_sdkconfig", ""))
+        core_variant_board = ''.join(variables.get("board_build.extra_flags", board_config.get("build.extra_flags", "")))
+        core_variant_board = core_variant_board.replace("-D", " ")
+        core_variant_build = (''.join(variables.get("build_flags", []))).replace("-D", " ")
         frameworks = variables.get("pioframework", [])
 
         def install_tool(TOOL, retry_count=0):
