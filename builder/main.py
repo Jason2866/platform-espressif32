b496803b6cf6437721cae419dcbed244be0f00c8
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2025-05-24 12:45:28 +0200

    Update main.py

--- builder/main.py
+++ builder/main.py
@@ -31,7 +31,7 @@ env = DefaultEnvironment()
 platform = env.PioPlatform()
 projectconfig = env.GetProjectConfig()
 IS_WINDOWS = sys.platform.startswith("win")
-terminal_cp = str(locale.getpreferredencoding())
+terminal_cp = str(locale.getpreferredencoding()).lower
 print("Terminal code page:",terminal_cp,"is set")
 
 #
