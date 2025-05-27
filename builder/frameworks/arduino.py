459e179062cdbdd91beb5855feecd120e564e2dc
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2025-05-27 20:18:53 +0200

    Update arduino.py

--- builder/frameworks/arduino.py
+++ builder/frameworks/arduino.py
@@ -644,9 +644,9 @@ if ("arduino" in pioframework and "espidf" not in pioframework and
     if flag_custom_component_remove:
         from component_manager import HandleCOMPONENTsettings, restore_pioarduino_build_py
         HandleCOMPONENTsettings(env, flag_custom_component_add, flag_custom_component_remove)           
-        silent_action = env.Action(restore_pioarduino_build_py())
-        silent_action.strfunction = lambda target, source, env: '' # hack to silence scons command output
-        env.AddPostAction("checkprogsize", silent_action)
+        #silent_action = env.Action(restore_pioarduino_build_py())
+        #silent_action.strfunction = lambda target, source, env: '' # hack to silence scons command output
+        env.AddPostAction("checkprogsize", restore_pioarduino_build_py())
     if IS_WINDOWS:
         # Smart include path optimization based on total path length
         env.AddBuildMiddleware(smart_include_length_shorten)
