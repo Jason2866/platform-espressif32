d8d8f3a772a595b6d750edb2975f0f854007f18c
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2025-05-31 17:56:53 +0200

    Update main.py

--- builder/main.py
+++ builder/main.py
@@ -636,27 +636,6 @@ elif upload_protocol == "esptool":
         env.VerboseAction("$UPLOADCMD", "Uploading $SOURCE"),
     ]
 
-# Configure upload protocol: DFU
-elif upload_protocol == "dfu":
-    hwids = board.get("build.hwids", [["0x2341", "0x0070"]])
-    vid = hwids[0][0]
-    pid = hwids[0][1]
-
-    upload_actions = [env.VerboseAction("$UPLOADCMD", "Uploading $SOURCE")]
-
-    env.Replace(
-        UPLOADER=join(
-            platform.get_package_dir("tool-dfuutil-arduino") or "", "dfu-util"
-        ),
-        UPLOADERFLAGS=[
-            "-d",
-            ",".join(["%s:%s" % (hwid[0], hwid[1]) for hwid in hwids]),
-            "-Q",
-            "-D",
-        ],
-        UPLOADCMD='"$UPLOADER" $UPLOADERFLAGS "$SOURCE"',
-    )
-
 # Configure upload protocol: Debug tools (OpenOCD)
 elif upload_protocol in debug_tools:
     _parse_partitions(env)
