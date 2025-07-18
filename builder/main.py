b1b981032cbc1a23014f50b78b079e2bba971e40
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2025-07-18 16:55:11 +0200

    Update main.py

--- builder/main.py
+++ builder/main.py
@@ -747,11 +747,11 @@ if mcu in ("esp32", "esp32s2", "esp32s3"):
     toolchain_arch = "xtensa-%s" % mcu
 
 # Set linker name and OBJDUMP name
-linker_name = "riscv32-esp-elf-clang-ld"
-objdump_name = "riscv32-esp-elf-clang-objdump"
+linker_name = "riscv32-esp-elf-clang"
+objdump_name = "riscv32-esp-elf-objdump"
 if mcu in ("esp32", "esp32s2", "esp32s3"):
-    linker_name = "xtensa-%s-elf-clang-ld" % mcu
-    objdump_name = "xtensa-%s-elf-clang-objdump" % mcu
+    linker_name = "xtensa-%s-elf-clang" % mcu
+    objdump_name = "xtensa-%s-elf-objdump" % mcu
 
 # Fix linker emulation for Clang by replacing xtensa linker flags
 def fix_clang_linkflags(linkflags):
