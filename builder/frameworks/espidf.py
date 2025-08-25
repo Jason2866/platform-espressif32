b04086b91f1c07f0c6b45d93c4ea9ee6ad918239
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2025-08-25 18:12:31 +0200

    Remove duplicate DefaultEnvironment setup
    Removed duplicate environment setup in espidf.py.

--- builder/frameworks/espidf.py
+++ builder/frameworks/espidf.py
@@ -46,15 +46,16 @@ from platformio.proc import exec_command
 from platformio.builder.tools.piolib import ProjectAsLibBuilder
 from platformio.package.version import get_original_version, pepver_to_semver
 
+
+env = DefaultEnvironment()
+env.SConscript("_embed_files.py", exports="env")
+platform = env.PioPlatform()
+
 penv_setup_path = os.path.join(platform.get_dir(), "builder")
 if penv_setup_path not in sys.path:
     sys.path.insert(0, penv_setup_path)
 
 from penv_setup import get_executable_path
-
-
-env = DefaultEnvironment()
-env.SConscript("_embed_files.py", exports="env")
 
 # remove maybe existing old map file in project root
 map_file = os.path.join(env.subst("$PROJECT_DIR"), env.subst("$PROGNAME") + ".map")
@@ -64,7 +65,6 @@ if os.path.exists(map_file):
 # Allow changes in folders of managed components
 os.environ["IDF_COMPONENT_OVERWRITE_MANAGED_COMPONENTS"] = "1"
 
-platform = env.PioPlatform()
 config = env.GetProjectConfig()
 board = env.BoardConfig()
 mcu = board.get("build.mcu", "esp32")
