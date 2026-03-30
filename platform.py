1479e9589db9796bf9a01323216ee5259a365df5
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2026-03-30 12:41:18 +0200

    allow python 3.14 for win

--- platform.py
+++ platform.py
@@ -17,10 +17,6 @@ import sys
 from platformio.compat import IS_WINDOWS
 
 pyver = sys.version_info
-if IS_WINDOWS:
-    allowed = (3, 10) <= pyver < (3, 14)
-    supported = "3.10, 3.11, 3.12, 3.13"
-else:
     allowed = (3, 10) <= pyver < (3, 15)
     supported = "3.10, 3.11, 3.12, 3.13, 3.14"
 if not allowed:
